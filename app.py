3113b5af9a91dbc523f061120e6f6258f91c340d
kmchale1974 <61988872+kmchale1974@users.noreply.github.com>
2025-07-23 13:19:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -86,5 +86,8 @@ def index():
 
     return render_template_string(TEMPLATE, events=events)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
